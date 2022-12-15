85c0dd2bb5abcbf09c20c660c5fd552158c5c065
Abdulaziz Abduvakhobov <abdulaziz.yosk@gmail.com>
2022-12-15 15:09:45 +0300

    Created template folders, added initial configs

--- club/settings.py
+++ club/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'club.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +128,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_URL = '/files/'
