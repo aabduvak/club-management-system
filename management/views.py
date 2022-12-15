fafcd52b080e43b01feb3c4b29d198106af2eb59
Abdulaziz Abduvakhobov <abdulaziz.yosk@gmail.com>
2022-12-16 02:01:26 +0300

    pushed untracked reset.html file

--- management/views.py
+++ management/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
 
 # Create your views here.
 class Index(View):
 	def get(self, request):
+		#if request.user.is_authenticated:
 		return render(request, 'management/index.html')
+		#return redirect(reverse('signin-page'))
 		
 class Signup(View):
 	def get(self, request):
